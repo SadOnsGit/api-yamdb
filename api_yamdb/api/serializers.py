484777279417da2ee05c414ea02aff043b95d641
Данила <danila-dvornikov12@mail.ru>
2025-09-06 15:08:04 +0300

    Написал мискины, Пермишины, сериализаторы, обновил юрлсы и написал вьюху

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+
+from reviews.models import (Category, Genre, Title)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    '''Сериализатор категорий.'''
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    '''Сериализатор жанров.'''
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug')
+
+
+class TitleWriteSerializer(serializers.ModelSerializer):
+    '''Сериализатор произведений.'''
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True,
+        required=False,
+    )
+
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all(),
+        many=False,
+        required=True
+    )
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'description', 'genre', 'category'
+        )
+
+
+class TitleViewSerializer(serializers.ModelSerializer):
+    '''Сериализатор произведений.'''
+    genre = GenreSerializer(many=True, required=False)
+    category = CategorySerializer(required=True,)
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
+        read_only_fields = (
+            'id', 'name', 'year', 'rating', 'description', 'genre', 'category'
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from reviews.models import Genre, Category, Title
+from .filters import TitleFilter
+from .mixins import MixinViewSet
+from .serializers import (
+    CategorySerializer,
+    GenreSerializer,
+    TitleViewSerializer,
+    TitleWriteSerializer,
+)
+from .permissions import IsAuthorOrModeratorOrAdminOrReadOnly
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    '''Вьюсет произведений.'''
+    queryset = Title.objects.all()
+    pagination_class = LimitOffsetPagination
+    permission_classes = (IsAuthorOrModeratorOrAdminOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.request.method in ('POST', 'PATCH'):
+            return TitleWriteSerializer
+        return TitleViewSerializer
+
+
+class GenreViewSet(MixinViewSet):
+    '''Вьюсет жанров.'''
+    queryset = Genre.objects.all()
+    pagination_class = LimitOffsetPagination
+    serializer_class = GenreSerializer
+    filter_backends = (filters.SearchFilter,)
+    permission_classes = (IsAuthorOrModeratorOrAdminOrReadOnly,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
+
+
+class CategoryViewSet(MixinViewSet):
+    '''Вьюсет категорий.'''
+    queryset = Category.objects.all()
+    pagination_class = LimitOffsetPagination
+    serializer_class = CategorySerializer
+    filter_backends = (filters.SearchFilter,)
+    permission_classes = (IsAuthorOrModeratorOrAdminOrReadOnly,)
+    search_fields = ('name',)
+    lookup_field = 'slug'
