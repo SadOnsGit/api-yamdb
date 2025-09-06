034b80a41b24920209a0f1c5bce1cfeb6f4d761f
YanaKachalova <yvk4x2@gmail.com>
2025-09-06 17:11:06 +0300

    Добавила расчёт рейтинга. Для этого залезла во вьюсет произведений, при слиянии веток надо аккуратнее смотреть.

--- api_yamdb/api/utils.py
+++
@@ -1,7 +0,0 @@
-from django.db.models import Avg, IntegerField
-from django.db.models.functions import Cast
-
-
-def with_title_rating(qs):
-    """Добавляет рейтинг (среднее по reviews.score)."""
-    return qs.annotate(rating=Cast(Avg('reviews__score'), IntegerField()))
