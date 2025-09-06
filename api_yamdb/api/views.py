1f446e9a3bbead96d22549d23083f5586a038c8c
YanaKachalova <yvk4x2@gmail.com>
2025-09-06 17:10:42 +0300

    Добавила расчёт рейтинга. Для этого залезла во вьюсет произведений, при слиянии веток надо аккуратнее смотреть.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,17 +1,27 @@
+from django.db.models import Avg, IntegerField
+from django.db.models.functions import Cast
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import AllowAny
+from rest_framework.pagination import LimitOffsetPagination
+
 
 from reviews.models import Review, Comment, Title
-from .serializers import ReviewSerializer, CommentSerializer
-from .permissions import IsAuthorModeratorAdminOrReadOnly
-
+from .permissions import IsAuthorOrModeratorOrAdminOrReadOnly
+from .filters import TitleFilter
+from .serializers import (
+    TitleViewSerializer,
+    TitleWriteSerializer,
+    ReviewSerializer,
+    CommentSerializer
+)
 
 class ReviewViewSet(ModelViewSet):
     """Сссылка: "/api/v1/titles/<title_id>/reviews/"."""
 
     serializer_class = ReviewSerializer
-    permission_classes = (IsAuthorModeratorAdminOrReadOnly,)
+    permission_classes = (IsAuthorOrModeratorOrAdminOrReadOnly,)
 
     def get_title(self):
         return get_object_or_404(Title, pk=self.kwargs.get("title_id"))
@@ -32,7 +42,7 @@ class CommentViewSet(ModelViewSet):
     """Ссылка: "/api/v1/titles/<title_id>/reviews/<review_id>/comments/"."""
 
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthorModeratorAdminOrReadOnly,)
+    permission_classes = (IsAuthorOrModeratorOrAdminOrReadOnly,)
 
     def get_review(self):
         review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
@@ -49,3 +59,26 @@ class CommentViewSet(ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user, review=self.get_review())
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    '''Вьюсет произведений.'''
+    pagination_class = LimitOffsetPagination
+    permission_classes = (IsAuthorOrModeratorOrAdminOrReadOnly,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+
+    def get_queryset(self):
+        return (
+            Title.objects
+            .select_related('category')
+            .prefetch_related('genre')
+            .annotate(
+                rating=Cast(Avg('reviews__score'), IntegerField())
+            )
+        )
+
+    def get_serializer_class(self):
+        if self.request.method in ('POST', 'PATCH'):
+            return TitleWriteSerializer
+        return TitleViewSerializer
