9d1674ec86ffd6632d1884ddb8ce40b44b92ae6b
YanaKachalova <yvk4x2@gmail.com>
2025-09-06 16:29:29 +0300

    Убрала дублирование проверки в пермишене. Добавила комментарий на тему проверки в два этапа.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,13 +3,23 @@ from rest_framework import permissions
 
 class IsAuthorAdminOrReadOnly(permissions.BasePermission):
 
+    @staticmethod
+    def _is_safe(request):
+        return request.method in permissions.SAFE_METHODS
+    # Проверка идёт в два этапа:
+    # 1) has_permission — глобально на уровне view (отвечаем на вопрос:
+    # "можно ли вообще выполнять запрос?").
+    # 2) has_object_permission — для конкретного объекта (отвечаем на вопрос:
+    # "можно ли именно к этому объекту?"").
+    # Поэтому метод для SAFE-запросов вызывается дважды.
+
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return request.user and request.user.is_authenticated
+        return self._is_safe(request) or (
+            request.user and request.user.is_authenticated
+        )
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if self._is_safe(request):
             return True
         user = request.user
         return (
